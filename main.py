eeb44ceb6b70aebb1afc824cf6bebf4b51303c36
Laddge <dev.laddge@gmail.com>
2021-04-26 13:19:31 +0900

    add head response

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import Response
 
 app = FastAPI()
+
+
+@app.middleware("http")
+async def head(request: Request, call_next):
+    if request.method == "HEAD":
+        response = Response()
+    else:
+        response = await call_next(request)
+    return response
 
 
 @app.get("/")
