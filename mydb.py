92a8a2135c57d2f0ccf6a5265a34d07a6918b3f3
Laddge <dev.laddge@gmail.com>
2021-04-27 18:30:56 +0900

    add reset func

--- mydb.py
+++ mydb.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from sqlalchemy import Column, DateTime, Integer, Text, create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -28,7 +29,7 @@ def get_class_ten():
         resdict[row.class_name] = {
             "status": row.status,
             "comment": row.comment,
-            "updated_at": row.updated_at
+            "updated_at": row.updated_at,
         }
     return resdict
 
@@ -37,7 +38,7 @@ def update_class_ten(class_name, status, comment, delete, updated_at):
     x = session.query(Class_Ten).get(class_name)
     x.status = status
     if delete:
-        x.comment = ''
+        x.comment = ""
     elif comment:
         x.comment = comment
     x.updated_at = updated_at
@@ -45,6 +46,17 @@ def update_class_ten(class_name, status, comment, delete, updated_at):
     return 0
 
 
+def reset_class_ten():
+    now = datetime.datetime.now()
+    result = session.query(Class_Ten).all()
+    for row in result:
+        row.status = 3
+        row.comment = ''
+        row.updated_at = now
+    session.commit()
+    return 0
+
+
 if __name__ == "__main__":
-    status, comment, updated_at = get_class_ten('21')
+    status, comment, updated_at = get_class_ten("21")
     print(status, comment, updated_at)
